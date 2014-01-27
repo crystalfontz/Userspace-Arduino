3912886338584ecbc0ce69a2753b59828f8dbabc
Anuj Deshpande <anujdeshpande92@gmail.com>
2014-01-27 15:38:16 +0530

    Update variants.cpp

--- libarduino/variants/cfa10036/variants.cpp
+++ libarduino/variants/cfa10036/variants.cpp
@@ -22,7 +22,7 @@ Arduino Uno format of pins
  *  13        |    GPIO    |            |    -       |            |    SCK     |            |
  *  -         |    GND     |            |    -       |            |    -       |            |
  *  -         |    AREF    |            |            |            |    -       |            |
- * // ADDITIONAL GPIO
+ * 
  *  A0        |    ADC     |            |            |            |    -       |            |
  *  A1        |    ADC     |            |            |            |    -       |            |
  *  A2        |    ADC     |            |            |            |    -       |            |
