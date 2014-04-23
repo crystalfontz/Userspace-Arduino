18e23491067749bd6860cb45d872e976f35e56d4
Mitul Vekariya <vekariya93@gmail.com>
2014-04-23 12:50:59 +0530

    Change ssp2 spidev path
    It's now /dev/spidev1.0

--- libarduino/variants/cfa10036/variant.h
+++ libarduino/variants/cfa10036/variant.h
@@ -39,7 +39,7 @@ extern "C"{
 *  You can use either spi, but dont forget to comment unused one!
 */
 
-#define LINUX_SPIDEV	"/dev/spidev1.1"	// SSP2
+#define LINUX_SPIDEV	"/dev/spidev1.0"	// SSP2
 //#define LINUX_SPIDEV	"/dev/spidev32766.1"	// SSP3
 
 typedef struct _PwmDescription
